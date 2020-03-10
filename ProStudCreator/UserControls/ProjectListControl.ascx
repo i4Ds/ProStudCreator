1397a43970d7260b3794887dacab9abc01abfd5a
Simon Beck <simubeck@hotmail.com>
2020-03-10 12:43:40 +0100

    added projectlistcontrol

---
+++ ProStudCreator/UserControls/ProjectListControl.ascx
@@ -0,0 +1,45 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="ProjectListControl.ascx.cs" Inherits="ProStudCreator.ProjectListControl" %>
+<asp:GridView ID="ProjectGrid" ItemType="ProStudCreator.ProjectRowElement" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None" AutoGenerateColumns="False" OnRowCommand="ProjectRowClick" OnRowDataBound="ProjectGrid_RowDataBound" AllowSorting="True" OnSorting="ProjectGrid_Sorting">
+    <%--<AlternatingRowStyle BackColor="White" />--%>
+    <Columns>
+        <asp:BoundField DataField="advisorName" HeaderText="Betreuer" SortExpression="Advisor" HtmlEncode="false" ItemStyle-Wrap="false"/>
+        <asp:BoundField DataField="projectNr" HeaderText="Projektnummer" SortExpression="ProjectNr"/>
+        <asp:BoundField DataField="projectName" HeaderText="Projektname" SortExpression="projectName" ItemStyle-Width="100%"/>
+        <asp:CheckBoxField HeaderText="P5" DataField="p5" SortExpression="P5"/>
+        <asp:CheckBoxField HeaderText="Lang" DataField="lng" SortExpression="Long"/>
+        <asp:CheckBoxField HeaderText="P6" DataField="p6" SortExpression="P6"/>
+        <asp:ImageField ControlStyle-CssClass="img-rounded imageHeight" DataImageUrlField="projectType1" HeaderText="Themen" ItemStyle-Width="20px"/>
+        <asp:ImageField ControlStyle-CssClass="img-rounded imageHeight" DataImageUrlField="projectType2" ItemStyle-Width="20px"/>
+        <asp:BoundField DataField="modDateString" HeaderText="Zuletzt geändert" SortExpression="modDate" />
+        <asp:TemplateField ItemStyle-Wrap="false">
+            <ItemTemplate>
+                <a title="PDF anzeigen" class="btn btn-default btnHeight glyphicon glyph-pdf" target="_blank" href="PDF?dl=false&id=<%# Item.id %>"></a>
+            </ItemTemplate>
+        </asp:TemplateField>
+        <asp:TemplateField ItemStyle-Wrap="false">
+            <ItemTemplate>
+                <a  class="btn btn-default btnHeight glyphicon glyphicon-info-sign" href="ProjectInfoPage?id=<%# Item.id %>"></a>
+            </ItemTemplate>
+        </asp:TemplateField>
+        <asp:TemplateField ItemStyle-Wrap="false">
+            <ItemTemplate>
+                <a title="Projekt bearbeiten" class="btn btn-default btnHeight glyphicon glyphicon-pencil" href="ProjectEditPage?id=<%# Item.id %>"></a>
+            </ItemTemplate>
+        </asp:TemplateField>
+        <asp:TemplateField ItemStyle-Wrap="false">
+            <ItemTemplate>
+                <asp:LinkButton runat="server" ID="deleteProjectButton" ToolTip="Projekt löschen" CommandName="deleteProject" OnClientClick="return confirm('Wollen Sie dieses Projekt wirklich löschen?');" CommandArgument="<%# Item.id %>" CssClass="btn btn-default btnHeight glyphicon glyphicon-remove"></asp:LinkButton>
+            </ItemTemplate>
+        </asp:TemplateField>
+    </Columns>
+    <EditRowStyle BackColor="#2461BF"/>
+    <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White"/>
+    <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White"/>
+    <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center"/>
+    <RowStyle BackColor="#EFF3FB"/>
+    <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333"/>
+    <SortedAscendingCellStyle BackColor="#F5F7FB"/>
+    <SortedAscendingHeaderStyle BackColor="#6D95E1"/>
+    <SortedDescendingCellStyle BackColor="#E9EBEF"/>
+    <SortedDescendingHeaderStyle BackColor="#4870BE"/>
+</asp:GridView>
---
+++ ProStudCreator/UserControls/ProjectListControl.ascx.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProStudCreator
+{
+    public partial class ProjectListControl : System.Web.UI.UserControl
+    {
+        private IQueryable<ProjectRowElement> Projects { get; set; }
+        public ProStudentCreatorDBDataContext db { get; set; }
+
+        public GridView Grid { get; private set; }
+
+        public bool ShowModificationDate { get; set; }
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            Grid = ProjectGrid;
+        }
+
+        public void SetProjects(IQueryable<Project> projects)
+        {
+            Projects = projects.Select(p => GetProjectRowElement(p));
+            Grid.DataSource = Projects;
+            Grid.DataBind();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Grid.Columns[8].Visible = ShowModificationDate;
+        }
+
+        private ProjectRowElement GetProjectRowElement(Project p)
+        {
+            return new ProjectRowElement
+            {
+                id = p.Id,
+                advisorName = string.Concat(new[]
+                {
+                    p.Advisor1 != null
+                        ? "<a href=\"mailto:" + p.Advisor1.Mail + "\">" +
+                            Server.HtmlEncode(p.Advisor1.Name).Replace(" ", "&nbsp;") + "</a>"
+                        : "?",
+                    p.Advisor2 != null
+                        ? "<br /><a href=\"mailto:" + p.Advisor2.Mail + "\">" +
+                            Server.HtmlEncode(p.Advisor2.Name).Replace(" ", "&nbsp;") + "</a>"
+                        : ""
+                }),
+                projectName = p.Name,
+                modDate = p.ModificationDate,
+
+                p5 = p.LogProjectType?.P5 ?? (p.POneType.P5 || (p.PTwoType?.P5 ?? false)),
+                p6 = p.LogProjectType?.P6 ?? (p.POneType.P6 || (p.PTwoType?.P6 ?? false)),
+                lng = p.LogProjectDuration == (byte)2,
+                projectType1 = "~/pictures/projectTyp" + (p.TypeDesignUX
+                                   ? "DesignUX"
+                                   : (p.TypeHW
+                                       ? "HW"
+                                       : (p.TypeCGIP
+                                           ? "CGIP"
+                                           : (p.TypeMlAlg
+                                               ? "MlAlg"
+                                               : (p.TypeAppWeb
+                                                   ? "AppWeb"
+                                                   : (p.TypeDBBigData
+                                                       ? "DBBigData"
+                                                       : (p.TypeSysSec
+                                                           ? "SysSec"
+                                                           : (p.TypeSE ? "SE" : "Transparent")))))))) + ".png",
+                projectType2 = "~/pictures/projectTyp" + (p.TypeHW && p.TypeDesignUX
+                                   ? "HW"
+                                   : (p.TypeCGIP && (p.TypeDesignUX || p.TypeHW)
+                                       ? "CGIP"
+                                       : (p.TypeMlAlg && (p.TypeDesignUX || p.TypeHW || p.TypeCGIP)
+                                           ? "MlAlg"
+                                           : (p.TypeAppWeb &&
+                                              (p.TypeDesignUX || p.TypeHW || p.TypeCGIP || p.TypeMlAlg)
+                                               ? "AppWeb"
+                                               : (p.TypeDBBigData &&
+                                                  (p.TypeDesignUX || p.TypeHW || p.TypeCGIP || p.TypeMlAlg ||
+                                                   p.TypeAppWeb)
+                                                   ? "DBBigData"
+                                                   : (p.TypeSysSec &&
+                                                      (p.TypeDesignUX || p.TypeHW || p.TypeCGIP || p.TypeMlAlg ||
+                                                       p.TypeAppWeb || p.TypeDBBigData)
+                                                       ? "SysSec"
+                                                       : (p.TypeSE && (p.TypeDesignUX || p.TypeHW || p.TypeCGIP ||
+                                                                       p.TypeMlAlg || p.TypeAppWeb ||
+                                                                       p.TypeDBBigData || p.TypeSysSec)
+                                                           ? "SE"
+                                                           : "Transparent"))))))) + ".png",
+                ProjectNr = p.GetProjectLabel()
+            };
+        }
+
+        protected void ProjectGrid_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                var project = db.Projects.Single(item => item.Id == ((ProjectRowElement)e.Row.DataItem).id);
+
+                if (!project.UserCanEdit())
+                {
+                    var x = e.Row.Cells[e.Row.Cells.Count - 3].Controls;
+                    e.Row.Cells[e.Row.Cells.Count - 2].Controls.OfType<DataBoundLiteralControl>().First().Visible = false; //edit
+                }
+
+                e.Row.Cells[e.Row.Cells.Count - 1].Controls.OfType<LinkButton>().First().Visible = project.UserCanDelete(); //delete
+
+                Color col = ColorTranslator.FromHtml(project.StateColor);
+                foreach (TableCell cell in e.Row.Cells)
+                    cell.BackColor = col;
+            }
+        }
+
+        protected void ProjectRowClick(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "Sort")
+            {
+                List<ProjectRowElement> sortedProjects = Projects.ToList();
+
+                switch (e.CommandArgument)
+                {
+                    case "Advisor":
+                        Grid.DataSource = sortedProjects.OrderBy(p => p.advisorName.Contains("?")).ThenBy(p => p.advisorName);
+                        break;
+                    case "ProjectNr":
+                        Grid.DataSource = sortedProjects.OrderBy(p => p.ProjectNr);
+                        break;
+                    case "projectName":
+                        Grid.DataSource = sortedProjects.OrderBy(p => p.projectName);
+                        break;
+                    case "P5":
+                        Grid.DataSource = sortedProjects.OrderByDescending(p => p.p5).ThenBy(p => p.lng);
+                        break;
+                    case "P6":
+                        Grid.DataSource = sortedProjects.OrderByDescending(p => p.p6);
+                        break;
+                    case "Long":
+                        Grid.DataSource = sortedProjects.OrderByDescending(p => p.lng);
+                        break;
+                    case "modDate":
+                        Grid.DataSource = sortedProjects.OrderByDescending(p => p.modDate);
+                        break;
+                }
+
+                Grid.DataBind();
+            }
+            else
+            {
+                var id = Convert.ToInt32(e.CommandArgument);
+                switch (e.CommandName)
+                {
+                    case "deleteProject":
+                        var project = db.Projects.Single(i => i.Id == id);
+                        project.Delete(db);
+                        db.SubmitChanges();
+                        Response.Redirect(Request.RawUrl);
+                        break;
+                    default:
+                        throw new Exception("Unknown command " + e.CommandName);
+                }
+            }
+        }
+        protected void ProjectGrid_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            Grid.DataBind();
+        }
+    }
+
+    public class ProjectRowElement
+    {
+        public int id { get; set; }
+        public string ProjectNr { get; set; }
+        public string advisorName { get; set; }
+        public string projectName { get; set; }
+        public string projectType1 { get; set; }
+        public string projectType2 { get; set; }
+        public bool p5 { get; set; }
+        public bool p6 { get; set; }
+        public bool lng { get; set; }
+        public DateTime modDate { get; set; }
+        public string modDateString { get => modDate.ToString("dd.MM.yyyy"); }
+    }
+}
