b89d0f9f3e44eb41232ac06b7f41882be592f9e8
Simon Felix <sf@iru.ch>
2025-06-16 17:14:51 +0200

    update dependencies

--- ProStudCreator/Global.asax.cs
+++ ProStudCreator/Global.asax.cs
@@ -38,18 +38,7 @@ namespace ProStudCreator
             Application.Add("dummyRequest", "");
 
             TaskHandler.CheckAllTasks();
-
-            //Solves problem which  occures if you installed a different language on your Pc 
-            //with out it the Encoding may not work on every Pc.
-            ICSharpCode.SharpZipLib.Zip.ZipConstants.DefaultCodePage = System.Text.Encoding.Default.CodePage;
-
-
-            
-
         }
-
-
-
 
         private void RegisterChacheEntry()
         {
@@ -60,9 +49,7 @@ namespace ProStudCreator
                 DateTime.MaxValue, TimeSpan.FromHours(3),
                 CacheItemPriority.Normal,
                 new CacheItemRemovedCallback(CacheItemRemovedCallback));
-
         }
-
 
         public void CacheItemRemovedCallback(string key, object value, CacheItemRemovedReason reason)
         {
